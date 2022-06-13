9b8ed5ef6cbcb10846551fc1b3141a0f2b8b2d5e
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-13 15:14:19 +0200

    SelectInput.vue created

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="selectSong">
+        <h3>FILTRA PER TIPO</h3>
+        <select>
+            <option value="0">all</option>
+            <option value="1">Rock</option> 
+            <option value="2">Pop</option> 
+            <option value="3">Jazz</option>
+            <option value="3">Metal</option>
+        </select>
+        <button id="mybutton">Click</button>
+        <input type="text" placeholder="Search" 
+            v-model="inputText" 
+            @keyup-enter="$emit('mySearch', inputText)">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectSongGenre',
+
+    data(){
+        return {
+            inputText : "",
+        }
+    }
+}
+</script>
+
+<style  scoped lang="scss">
+
+    .selectSong {
+        padding: 20px;
+        text-align: center;
+
+        h3 {
+            color: white;
+            margin-bottom: 20px;
+        }
+
+        button {
+            margin: 0px 30px;
+        }
+    }
+    
+</style>
