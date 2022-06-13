50399e9c25d765c7a8f75cddcf873ec3222e1a5c
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-13 09:14:39 +0200

    Created component Load

---
+++ src/components/Load.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Caricamento immagini in corso...</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoadDiscs'
+}
+</script>
+
+<style  scoped lang="scss">
+
+div {
+    padding: 20px;
+    border-radius: 20px;
+}
+</style>
